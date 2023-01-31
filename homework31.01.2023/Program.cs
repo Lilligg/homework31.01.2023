7dd0d86929a07bba4706bdb09eb44b1a0c0e9f12
lidao <lidao@DESKTOP-ITSV686>
2023-01-31 15:57:58 +0300

    Добавьте файлы проекта.

---
+++ homework31.01.2023/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using homework31._01._2023;
+using System.Runtime.InteropServices;
+
+string[] names = {"Пенси", "Драко", "Тео", "Блейз", "Гермиона"};
+User user = new User();
+Random rand = new Random();
+for (int i = 0; i < names.Length; i++)
+{
+    user.name = names[i];
+    user.age = rand.Next(0, 100);
+    user.Print();
+}
---
+++ homework31.01.2023/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework31._01._2023
+{
+    public class User
+    {
+        public string name = "Undefined";
+        public int age;
+        public void Print()
+        {
+            Console.WriteLine($"Имя {name}  Возраст {age}");
+        }
+    }
+}
